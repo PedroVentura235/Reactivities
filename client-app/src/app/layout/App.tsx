e5c8fb283d04775b2f873735baa82b1097a4335a
Pedro Ventura <pedroventurawork@gmail.com>
2022-03-30 20:48:41 +0100

    End of section 13

--- client-app/src/app/layout/App.tsx
+++ client-app/src/app/layout/App.tsx
@@ -1,23 +1,39 @@
-import  { Fragment } from 'react';
+import  { Fragment, useEffect } from 'react';
 import {  Container } from 'semantic-ui-react';
 import { ToastContainer } from 'react-toastify';
 import NavBar from './NavBar';
 import ActivityDashboard from '../../features/activities/dashboard/ActivityDashboard';
 import { observer } from 'mobx-react-lite';
 import { Route, Switch, useLocation } from 'react-router-dom';
-import HomePage from '../../features/activities/home/HomePage';
+import HomePage from '../../features/home/HomePage';
 import ActivityForm from '../../features/activities/form/ActivityForm';
 import ActivityDetails from '../../features/activities/details/ActivityDetails';
-import TestErrors from '../../features/activities/errors/TestError';
-import NotFound from '../../features/activities/errors/NotFound';
-import ServerError from '../../features/activities/errors/ServerError';
+import TestErrors from '../../features/errors/TestError';
+import NotFound from '../../features/errors/NotFound';
+import ServerError from '../../features/errors/ServerError';
+import LoginForm from '../../features/users/LoginForm';
+import { useStore } from '../stores/store';
+import LoadingComponent from './LoadingComponent';
+import ModalContainer from '../common/modals/ModalContainer';
 
 function App() {
   const location = useLocation();
+  const {commonStore, userStore} = useStore();
+
+  useEffect(() => {
+    if(commonStore.token) {
+      userStore.getUser().finally(() => commonStore.setAppLoaded());
+    } else {
+       commonStore.setAppLoaded();
+    }
+  }, [commonStore, userStore])
+
+  if(!commonStore.appLoaded) return <LoadingComponent content='Loading App...'/> 
 
   return (
     <Fragment>
       <ToastContainer position='bottom-right' hideProgressBar/>
+      <ModalContainer />
       <Route exact path='/' component={HomePage} />
       <Route 
         path={'/(.+)'}
@@ -31,6 +47,7 @@ function App() {
                 <Route key={location.key} path={['/createActivity', '/manage/:id']} component={ActivityForm} />
                 <Route path='/errors' component={TestErrors} />
                 <Route path='/server-error' component={ServerError} />
+                <Route path='/login' component={LoginForm} />
                 <Route component={NotFound} />
               </Switch>
             </Container>
